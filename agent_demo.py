cae50e892ce0548749d5f8fb361ad750bd38798d
sminuku <minukurisushmitha@gmail.com>
2026-07-26 16:08:49 +0530

    commenting bkp files

--- agent_demo.py
+++ agent_demo.py
@@ -1,21 +1,21 @@
 
-from langgraph.graph import END, START, MessagesState
-from langgraph.graph import StateGraph
-from langchain.messages import HumanMessage
+# from langgraph.graph import END, START, MessagesState
+# from langgraph.graph import StateGraph
+# from langchain.messages import HumanMessage
 
-def greeting(state: MessagesState) -> MessagesState:
-    return {"messages": [HumanMessage(content="Hello! How can I assist you today?")]}
+# def greeting(state: MessagesState) -> MessagesState:
+#     return {"messages": [HumanMessage(content="Hello! How can I assist you today?")]}
 
 
-builder = StateGraph(MessagesState)
-builder.add_node("greeting", greeting)
-builder.set_entry_point("greeting")
-builder.add_edge(START, "greeting")
-builder.add_edge("greeting", END)
+# builder = StateGraph(MessagesState)
+# builder.add_node("greeting", greeting)
+# builder.set_entry_point("greeting")
+# builder.add_edge(START, "greeting")
+# builder.add_edge("greeting", END)
 
-graph  = builder.compile()
+# graph  = builder.compile()
 
-response = graph.invoke({"messages": [HumanMessage(content="Hi there!")]})
+# response = graph.invoke({"messages": [HumanMessage(content="Hi there!")]})
 
-for message in response["messages"]:
-    print(message.content)
+# for message in response["messages"]:
+#     print(message.content)
--- supervisor-agent.py
+++ supervisor-agent.py
@@ -1,65 +1,65 @@
 
-from typing import Any
-from langgraph.graph import END, START, MessagesState
-from langgraph.graph import StateGraph
-from langchain.messages import HumanMessage
-from code_generator import generate
-from code_explanation import explain
-from agent_state import AgentState
+# from typing import Any
+# from langgraph.graph import END, START, MessagesState
+# from langgraph.graph import StateGraph
+# from langchain.messages import HumanMessage
+# from code_generator import generate
+# from code_explanation import explain
+# from agent_state import AgentState
 
 
-def generator_node(state: AgentState) -> dict[str, Any]:
-    result = generate(state)
-    return {
-        "output_code": result
-    }
+# def generator_node(state: AgentState) -> dict[str, Any]:
+#     result = generate(state)
+#     return {
+#         "output_code": result
+#     }
 
-def explain_node(state: AgentState) -> dict[str, Any]:
-    explanation = explain(state)
-    return {
-        "explanation": explanation
-    }
+# def explain_node(state: AgentState) -> dict[str, Any]:
+#     explanation = explain(state)
+#     return {
+#         "explanation": explanation
+#     }
 
 
-builder = StateGraph(AgentState)
-builder.add_node("generator_node", generator_node)
-builder.add_node("explain_node", explain_node)
-builder.set_entry_point("generator_node")
-builder.add_edge(START, "generator_node")
-builder.add_edge("generator_node", "explain_node")
-builder.add_edge("explain_node", END)
+# builder = StateGraph(AgentState)
+# builder.add_node("generator_node", generator_node)
+# builder.add_node("explain_node", explain_node)
+# builder.set_entry_point("generator_node")
+# builder.add_edge(START, "generator_node")
+# builder.add_edge("generator_node", "explain_node")
+# builder.add_edge("explain_node", END)
 
-graph  = builder.compile()
+# graph  = builder.compile()
 
-query = "Please write a Python function to calculate the sum of two numbers."
-response = graph.invoke({
-    "messages": [HumanMessage(content=query)],
-    "query": query,
-    "translation": False,
-    "source_language": "",
-    "target_language": "",
-    "source_code": "",
-    "output_code": "",
-    "explanation": ""
-})
+# query = "Please write a Python function to calculate the sum of two numbers."
+# response = graph.invoke({
+#     "messages": [HumanMessage(content=query)],
+#     "query": query,
+#     "translation": False,
+#     "source_language": "",
+#     "target_language": "",
+#     "source_code": "",
+#     "output_code": "",
+#     "explanation": ""
+# })
 
-for message in response["messages"]:
-    print(message.content)
+# for message in response["messages"]:
+#     print(message.content)
 
-print("Output Code: \n")    
-print(response["output_code"])
-print("Explanation: \n")
-print(response["explanation"])
+# print("Output Code: \n")    
+# print(response["output_code"])
+# print("Explanation: \n")
+# print(response["explanation"])
 
-def invoke_graph(query: str) -> dict[str, Any]:
-    response = graph.invoke({
-        "messages": [HumanMessage(content=query)],
-        "query": query,
-        "translation": False,
-        "source_language": "",
-        "target_language": "",
-        "source_code": "",
-        "output_code": "",
-        "explanation": ""
-    })
-    return response
+# def invoke_graph(query: str) -> dict[str, Any]:
+#     response = graph.invoke({
+#         "messages": [HumanMessage(content=query)],
+#         "query": query,
+#         "translation": False,
+#         "source_language": "",
+#         "target_language": "",
+#         "source_code": "",
+#         "output_code": "",
+#         "explanation": ""
+#     })
+#     return response
